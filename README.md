8eae443b5b9f3d55c32d0ea9b9793aec7a0e72ce
Michael D Pendergrass <mdpendergrass@gmail.com>
2016-03-13 16:51:00 -0400

    move project to leiningen

---
+++ README.md
@@ -0,0 +1,15 @@
+# stateful-sessions
+
+An extraction of the stateful-sessions code from Brenton Ashworth's seemingly abandoned sandbar library.  I had previously used sandbar in some of my projects but could not easily release and tag those projects with maven due to the sandbar dependency being a SNAPSHOT version.  Also, the dependencies being used by sandbar were older versions of ring, hiccup, etc.  So, I factored out the parts I needed.  The same copyright (Eclipse) applies as the original work by Mr. Ashworth.  Use as is and at your own risk.  Improvements are welcome.
+
+## Usage
+
+```
+[org.pupcus/stateful-sessions "0.0.2"]
+```
+
+## License
+
+stateful-sessions is distributed under the [Eclipse Public License](http://opensource.org/licenses/eclipse-1.0.php), the same as Clojure.
+
+Use at your own risk. 
---
+++ project.clj
@@ -0,0 +1,26 @@
+(defproject org.pupcus/stateful-sessions "0.0.2-SNAPSHOT"
+
+  :description "sandbar's stateful sessions factored out"
+
+  :url "https://bitbucket.org/pupcus/stateful-sessions"
+
+  :scm {:url "git@bitbucket.org:pupcus/stateful-sessions"}
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[ring "1.4.0"]]
+
+  :profiles {:dev  {:resource-paths ["dev-resources"]
+                    :dependencies [[org.clojure/clojure "1.8.0"]
+                                   [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
+
+  :deploy-repositories [["snapshots"
+                         {:url "https://clojars.org/repo"
+                          :creds :gpg}]
+                        ["releases"
+                         {:url "https://clojars.org/repo"
+                          :creds :gpg}]]
+
+  :global-vars {*warn-on-reflection* true
+                *assert* false})
