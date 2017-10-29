2498cc36a14696e6bebffadb47fda446d29d4f9a
Michael D Pendergrass <mdpendergrass@gmail.com>
2017-10-29 14:02:48 -0400

    update dependencies and prepare for release

--- project.clj
+++ project.clj
@@ -9,18 +9,30 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[ring "1.4.0"]]
+  :dependencies [[ring "1.6.2"]]
 
   :profiles {:dev  {:resource-paths ["dev-resources"]
                     :dependencies [[org.clojure/clojure "1.8.0"]
-                                   [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
+                                   [org.slf4j/slf4j-log4j12 "1.7.25"]]}}
 
   :deploy-repositories [["snapshots"
                          {:url "https://clojars.org/repo"
+                          :sign-releases false
                           :creds :gpg}]
                         ["releases"
                          {:url "https://clojars.org/repo"
+                          :sign-releases false
                           :creds :gpg}]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
 
   :global-vars {*warn-on-reflection* true
                 *assert* false})
